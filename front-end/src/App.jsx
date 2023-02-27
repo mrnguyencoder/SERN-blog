cf1b9d8c4feb1391acc7094caff8460dcbc53cc8
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-27 14:20:34 +0100

    react-router setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,60 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Createblog from "./pages/Createblog";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Showblog from "./pages/Showblog";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Showblog />,
+      },
+      {
+        path: "/write",
+        element: <Createblog />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="">
-      <p className="">Blog</p>
+      <RouterProvider router={router}/>
     </div>
   )
 }
