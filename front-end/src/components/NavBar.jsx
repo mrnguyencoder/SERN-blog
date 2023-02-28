0c275ebce0104e2e5f991b5f5fb3620e319b1975
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-28 13:10:46 +0100

    front

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -51,7 +51,7 @@ function NavBar() {
                     href="#"
                     className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
-                    Blog
+                    Login
                   </a>
                 </div>
               </div>
@@ -141,7 +141,7 @@ function NavBar() {
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="#home"
                 className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
               >
                 Home
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
         // More posts...
       ]
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div id="home" className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
